ea6cb15746e42fa9cba53609c6f4be16c76c5d68
Kidō <chikyu.kido@posteo.de>
2026-01-30 22:42:27 +0100

    use Long2LongMap for the LastInteractionSystem to improve performance

--- src/main/java/io/github/chikyukido/lovelystats/systems/LastInteractionSystem.java
+++ src/main/java/io/github/chikyukido/lovelystats/systems/LastInteractionSystem.java
@@ -5,29 +5,35 @@ import com.hypixel.hytale.protocol.packets.player.ClientMovement;
 import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
 import com.hypixel.hytale.server.core.io.adapter.PacketAdapters;
 import com.hypixel.hytale.server.core.io.adapter.PlayerPacketWatcher;
+import it.unimi.dsi.fastutil.longs.Long2LongMap;
+import it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap;
 
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class LastInteractionSystem {
-    private static final Map<UUID, Long> lastInteraction = new ConcurrentHashMap<>();
+    private static final Long2LongMap lastInteraction = new Long2LongOpenHashMap();
     private static final long IDLE_MS = 5 * 60 * 1000;
+
+    static {
+        lastInteraction.defaultReturnValue(Long.MAX_VALUE);
+    }
 
     public static void registerLastInteractionSystem() {
         PacketAdapters.registerInbound((PlayerPacketWatcher) (playerRef, packet) -> {
             if (packet instanceof ClientMovement || packet instanceof SyncInteractionChains) {
-                lastInteraction.put(playerRef.getUuid(), System.currentTimeMillis());
+               long msb = playerRef.getUuid().getMostSignificantBits();
+                lastInteraction.put(msb, System.currentTimeMillis());
             }
         });
     }
 
     public static boolean isPlayerIdle(UUID uuid) {
-        long last = lastInteraction.getOrDefault(uuid, Long.MAX_VALUE);
+        long last = lastInteraction.get(uuid.getMostSignificantBits());
         return System.currentTimeMillis() - last > IDLE_MS;
     }
 
     public static void onPlayerConnect(PlayerConnectEvent event) {
-        lastInteraction.put(event.getPlayerRef().getUuid(), System.currentTimeMillis());
+        long msb = event.getPlayerRef().getUuid().getMostSignificantBits();
+        lastInteraction.put(msb, System.currentTimeMillis());
     }
 }
